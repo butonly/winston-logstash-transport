9adc257fa79c19745c6c9a724e1cc518ca91eae3
liuyanjie <lyj8888888888@gmail.com>
2018-06-23 16:15:22 +0800

    feat(): add winston-logstash-transport

---
+++ index.js
@@ -0,0 +1,65 @@
+
+const dgram = require('dgram')
+const os = require('os')
+
+const winston = require('winston')
+
+/* eslint-disable no-empty-function */
+function noop() {}
+/* eslint-enable no-empty-function */
+
+class LogstashTransport extends winston.Transport {
+  constructor(options) {
+    options = options || {}
+
+    super(options)
+
+    this.name = 'LogstashTransport'
+    this.pid = process.pid
+
+    this.host = options.host
+    this.port = options.port
+    this.hostname = options.hostname || os.hostname()
+    this.application = options.application
+    this.trailingLineFeed = options.trailingLineFeed === true
+    this.trailingLineFeedChar = options.trailingLineFeedChar || os.EOL
+    this.silent = options.silent
+
+    this.client = null
+
+    this.connect()
+  }
+
+  connect() {
+    this.client = dgram.createSocket('udp4')
+    this.client.unref()
+  }
+
+  log(info, callback) {
+    if (this.silent) {
+      return callback(null, true)
+    }
+
+    info.message.level = info[Symbol.for('level')]
+    info.message.application = this.application
+    info.message.hostname = this.hostname
+    info.message.pid = this.pid
+    info.message.time = new Date()
+
+    this.send(JSON.stringify(info.message), (err) => {
+      this.emit('logged', !err)
+      callback(err, !err)
+    })
+  }
+
+  send(message, callback) {
+    if (this.trailingLineFeed === true) {
+      message = message.replace(/\s+$/, '') + this.trailingLineFeedChar
+    }
+
+    const buf = Buffer.from(message)
+    this.client.send(buf, 0, buf.length, this.port, this.host, (callback || noop))
+  }
+}
+
+exports.LogstashTransport = LogstashTransport
